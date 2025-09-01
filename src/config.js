bd11e5f2d45c939b6f0339ee90f981679eb5afdf
Rishabh Chaudhary <rishabhchaudhary058@gmail.com>
2025-09-01 17:14:34 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://medical-booking-app.onrender.com";
+export const BASE_URL = "https://medical-booking-app.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
